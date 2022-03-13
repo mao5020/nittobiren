56d1b0db8a48143dc2a63ecb8dc1d6e8a6d985c4
kenta <>
2022-03-14 01:14:05 +0900

    都道県連一覧追加

---
+++ wp/nittobiren/wp-content/themes/nittobiren/page-prefectures.php
@@ -0,0 +1,243 @@
+<?php get_header(); ?>
+<div class="page_cover">
+  <h1 class="page_ttl">都道県連一覧</h1>
+</div>
+<?php get_template_part('p-header'); ?>
+</body>
+</html>
+
+<div class="flex_inner">
+  <?php get_sidebar();?>
+  <div class="l_inner">
+    <section class="prefectures sec_inner">
+      <h2 class="h2_ttl">都道県連名簿</h2>
+      <div class="l_table">
+        <p class="l_table_row">第1ブロック</p>
+        <p class="l_table_data">
+          <a href="#hokkaido" class="prefectures-table-link">北海道</a>
+          <a href="#" class="prefectures-table-link">青森県</a>
+          <a href="#" class="prefectures-table-link">岩手県</a>
+          <a href="#" class="prefectures-table-link">秋田県</a>
+          <a href="#" class="prefectures-table-link">宮城県</a>
+          <a href="#" class="prefectures-table-link">山形県</a>
+          <a href="#" class="prefectures-table-link">福島県</a>
+        </p>
+      </div>
+      <div class="l_table">
+        <p class="l_table_row">第2ブロック</p>
+        <p class="l_table_data">
+          <a href="#" class="prefectures-table-link">栃木県</a>
+          <a href="#" class="prefectures-table-link">群馬県</a>
+          <a href="#" class="prefectures-table-link">千葉県</a>
+          <a href="#" class="prefectures-table-link">東京都</a>
+          <a href="#" class="prefectures-table-link">静岡県</a>
+          <a href="#" class="prefectures-table-link">新潟県</a>
+        </p>
+      </div>
+      <div class="l_table">
+        <p class="l_table_row">第3ブロック</p>
+        <p class="l_table_data">
+          <a href="#" class="prefectures-table-link">愛知県</a>
+          <a href="#" class="prefectures-table-link">富山県</a>
+          <a href="#" class="prefectures-table-link">石川県</a>
+          <a href="#" class="prefectures-table-link">福井県</a>
+          <a href="#" class="prefectures-table-link">兵庫県</a>
+          <a href="#" class="prefectures-table-link">和歌山県</a>
+      </p>
+      </div>
+      <div class="l_table">
+        <p class="l_table_row">第4ブロック</p>
+        <p class="l_table_data">
+          <a href="#" class="prefectures-table-link">香川県</a>
+          <a href="#" class="prefectures-table-link">徳島県</a>
+          <a href="#" class="prefectures-table-link">高知県</a>
+          <a href="#" class="prefectures-table-link">愛媛県</a>
+          <a href="#" class="prefectures-table-link">広島県</a>
+          <a href="#" class="prefectures-table-link">島根県</a>
+          <a href="#" class="prefectures-table-link">鳥取県</a>
+          <a href="#" class="prefectures-table-link">岡山県</a>
+          <a href="#" class="prefectures-table-link">山口県</a>
+        </p>
+      </div>
+      <div class="l_table">
+        <p class="l_table_row">第5ブロック</p>
+        <p class="l_table_data">
+          <a href="#" class="prefectures-table-link">福岡県</a>
+          <a href="#" class="prefectures-table-link">佐賀県</a>
+          <a href="#" class="prefectures-table-link">長崎県</a>
+          <a href="#" class="prefectures-table-link">熊本県</a>
+          <a href="#" class="prefectures-table-link">大分県</a>
+          <a href="#" class="prefectures-table-link">宮崎県</a>
+          <a href="#" class="prefectures-table-link">鹿児島県</a>
+          <a href="#" class="prefectures-table-link">沖縄県</a>
+        </p>
+      </div>
+    </section>
+
+    <section class="prefectures sec_inner">
+      <h2 class="h2_ttl">第1ブロック</h2>
+      <div class="l_chart">
+        <p class="l_chart_row">北海道鳶工業会</p>
+        <p class="l_chart_data">〒003-0806<br>札幌市白石区菊水6条2丁目1-15　札幌鳶組合内<br>TEL：011-822-5635／FAX：011-822-9487</p>
+      </div>
+      <div class="l_chart">
+        <a class="prefectures-link" href="http://www.aochuken.or.jp/" target="_blank">（一社）青森県中小建設業協会</a>
+        <p class="l_chart_data">〒030-0822<br>青森市中央1-13-11　アパートしうら101号<br>TEL：017-723-7800／FAX：017-718-1278</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">岩手県鳶工業連合会</p>
+        <p class="l_chart_data">〒020-0173<br>滝沢市木賊川410-5　㈲鳶釜石組内<br>TEL：019-688-6688／FAX：019-688-6689</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">秋田県鳶土木連合会</p>
+        <p class="l_chart_data">〒010-0951<br>秋田市山王6丁目4-11　渡部組ビル3階<br>TEL：018-823-6213／FAX：018-823-6214</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">宮城県鳶土工業連合会</p>
+        <p class="l_chart_data">〒987-2308<br>栗原市一迫真坂字中町27　㈲門伝建設内<br>TEL：0228-52-4726／FAX：0228-52-4779</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">山形県鳶工業連合会</p>
+        <p class="l_chart_data">〒991-0061<br>寒河江市中央工業団地176-10　大沼工業㈱内<br>TEL：0237-84-2463／FAX：0237-85-0855</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">福島県鳶土木工業連合会</p>
+        <p class="l_chart_data">〒963-8024<br>郡山市朝日1丁目27-8　㈲宍戸建設内<br>TEL：024-923-4150／FAX：024-923-4684</p>
+      </div>
+    </section>
+
+    <section class="prefectures sec_inner">
+      <h2 class="h2_ttl">第2ブロック</h2>
+      <div class="l_chart">
+        <p class="l_chart_row">栃木県鳶工業連合会</p>
+        <p class="l_chart_data">〒321-0157<br>宇都宮市幕田町383-2　㈱大勝建設内<br>TEL：028-616-6202／FAX：028-616-6203</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">（一社）群馬県鳶工業連合会</p>
+        <p class="l_chart_data">〒370-0078<br>高崎市上小鳥町439-8　福田組内<br>TEL：027-361-8611／FAX：027-363-7053</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">（一社）千葉県鳶工業会</p>
+        <p class="l_chart_data">〒270-1416<br>白井市神々廻876　㈲石田組内<br>TEL：047-404-8906／FAX：047-404-8907</p>
+      </div>
+      <div class="l_chart">
+        <a class="prefectures-link" href="http://www.tokyotobi.or.jp/" target="_blank">（一社）東京都鳶工業会</a>
+        <p class="l_chart_data">〒105-0011<br>港区芝公園3-5-20　日鳶連会館4階<br>TEL：03-6435-9022／FAX：03-6435-9023</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">静岡県鳶工業会</p>
+        <p class="l_chart_data">〒411-0807<br>三島市竹倉286-1<br>TEL：055-972-5145／FAX：055-975-9610</p>
+      </div>
+      <div class="l_chart">
+        <a class="prefectures-link" href="https://www.chuokai-niigata.or.jp/nagaokatobi/index.html" target="_blank">新潟県鳶土工職組合連合会</a>
+        <p class="l_chart_data">〒940-0092<br>長岡市昭和2-1-15　鳶協会館内<br>TEL：0258-36-6310／FAX：0258-36-8681<br>※長岡鳶工業協同組合ホームページへのリンクになります</p>
+      </div>
+    </section>
+
+    <section class="prefectures sec_inner">
+      <h2 class="h2_ttl">第3ブロック</h2>
+      <div class="l_chart">
+        <p class="l_chart_row">愛知県鳶土工業組合連合会</p>
+        <p class="l_chart_data">〒444-0115<br>額田郡幸田町大字荻字研山11　㈱山本組内<br>TEL：0564-62-0520／FAX：0564-62-0407</p>
+      </div>
+      <div class="l_chart">
+        <a class="prefectures-link" href="http://toyama-tobi.jp/" target="_blank">富山県鳶土工業協同組合</a>
+        <p class="l_chart_data">〒930-0108<br>富山市本郷中部74　呉羽保温工業ビル2階<br>TEL：076-436-1821／FAX：076-436-1822</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">石川県鳶工業会</p>
+        <p class="l_chart_data">〒921-8036<br>金沢市弥生2-1-23　石川県建設総合センター内<br>TEL：076-243-8642／FAX：076-243-8642</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">福井県鳶土工業協同組合</p>
+        <p class="l_chart_data">〒910-0853<br>福井市城東4-22-20<br>TEL：0776-26-1125／FAX：0776-26-1144</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">兵庫県鳶土工連合会</p>
+        <p class="l_chart_data">〒671-0242<br>姫路市四郷町中鈴22-1　上杉工業㈱内<br>TEL：079-251-7117／FAX：079-251-7118</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">和歌山県鳶工業連合会</p>
+        <p class="l_chart_data">〒640-8303<br>和歌山市鳴神697　㈲山田工業内<br>TEL：073-473-4813／FAX：073-473-4831</p>
+      </div>
+    </section>
+
+    <section class="prefectures sec_inner">
+      <h2 class="h2_ttl">第4ブロック</h2>
+      <div class="l_chart">
+        <a class="prefectures-link" href="https://www.tobiren.com/" target="_blank">香川県鳶土工連合協会</a>
+        <p class="l_chart_data">〒763-0086<br>丸亀市飯野町西分甲505-1HKKフィール３F－G<br>TEL：0877-43-2050／FAX：0877-22-9594</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">徳島県鳶土工連合会</p>
+        <p class="l_chart_data">〒776-0001<br>吉野川市鴨島町牛島1572番地1　㈱松島組内<br>TEL：0883-24-1233／FAX：0883-24-0710</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">高知県鳶工業連合会</p>
+        <p class="l_chart_data">〒781-0302<br>高知市春野町弘岡中1016-8　黒潮興業㈱内<br>TEL：088-803-9640／FAX：088-803-9641</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">愛媛県鳶土工業連合会</p>
+        <p class="l_chart_data">〒791-8057<br>松山市大可賀2-3-17　㈱福崎組内<br>TEL：089-951-0730／FAX：089-951-6779</p>
+      </div>
+      <div class="l_chart">
+        <a class="prefectures-link" href="http://www.hiroshimatobi.jp/" target="_blank">（一社）広島県鳶土工業連合会</a>
+        <p class="l_chart_data">〒720-0836<br>福山市瀬戸町長和686番地1　㈱田畑技建工業内<br>TEL：084-944-7880／FAX：084-944-7881</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">島根県鳶工業連合会</p>
+        <p class="l_chart_data">〒692-0047<br>安来市早田町295-4　㈱シオミ内<br>TEL：0854-23-7557／FAX：0854-23-7558</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">鳥取県鳶土工協会</p>
+        <p class="l_chart_data">〒689-0105<br>鳥取市福部町湯山1300-5　㈱徳永組内<br>TEL：0857-38-1090／FAX：0857-38-1091</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">岡山県鳶土工連合会</p>
+        <p class="l_chart_data">〒710-0023<br>倉敷市帯高171-1F　(株)隼ビルド内<br>TEL：086-420-1122／FAX：086-420-1133</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">山口県鳶工業連合会</p>
+        <p class="l_chart_data">〒757-0216<br>宇部市大字船木1236　㈲栗栖工業内<br>TEL：0836-69-0707／FAX：0836-69-0708</p>
+      </div>
+    </section>
+
+    <section class="prefectures sec_inner">
+      <h2 class="h2_ttl">第5ブロック</h2>
+      <div class="l_chart">
+        <p class="l_chart_row">福岡県鳶土工業連合会</p>
+        <p class="l_chart_data">〒810-0022<br>福岡市中央区薬院1-6-5　ホワイティ薬院506号<br>TEL：092-406-6776／FAX：092-406-6701</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">佐賀県鳶土工工事業連合会</p>
+        <p class="l_chart_data">〒840-0201<br>佐賀市大和町尼寺788-1　安部建設㈱内<br>TEL：0952-62-0378／FAX：0952-62-0838</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">長崎県鳶土工業連合会</p>
+        <p class="l_chart_data">〒852-8031<br>長崎市三芳町8-3　奥田建設㈱内<br>TEL：095-844-9178／FAX：095-844-9179</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">熊本県鳶工業組合連合会</p>
+        <p class="l_chart_data">〒862-0976<br>熊本市中央区九品寺1-14-11<br>TEL：096-277-1550／FAX：096-277-1588</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">大分県鳶土工業連合会</p>
+        <p class="l_chart_data">〒870-0116<br>大分市常行140-2　㈲大昇工業内<br>TEL：097-523-3651／FAX：097-527-4766</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">宮崎県鳶土工業連合会</p>
+        <p class="l_chart_data">〒885-0112<br>都城市乙房町130-1　㈱徳丸開発内<br>TEL：0986-37-3255／FAX：0986-37-3266</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">鹿児島県鳶土工業連合会</p>
+        <p class="l_chart_data">〒891-0151<br>鹿児島市光山2丁目3番46号　㈱中西組内<br>TEL：099-261-7777／FAX：099-261-7710</p>
+      </div>
+      <div class="l_chart">
+        <p class="l_chart_row">沖縄県鳶土工業会</p>
+        <p class="l_chart_data">〒901-2221<br>宜野湾市伊佐2-20-7　㈲平良組内<br>TEL：098-898-1747／FAX：098-898-1749</p>
+      </div>
+    </section>
+    
+  </div>  <!-- /l_inner -->
+</div>
+<?php get_footer(); ?>
