e40a17384ef9ad136deb8732318f17d9bd2cf0c3
kenta <>
2022-03-19 21:53:08 +0900

    基幹

---
+++ wp/nittobiren/wp-content/themes/nittobiren/page-kikan.php
@@ -0,0 +1,65 @@
+<?php get_header(); ?>
+<div class="page_cover">
+  <h1 class="page_ttl">登録基幹技能講習</h1>
+</div>
+<?php get_template_part('p-header'); ?>
+</body>
+</html>
+
+<div class="flex_inner">
+  <?php get_sidebar();?>
+  <div class="l_inner">
+    <section class="kikan sec_inner">
+      <h2 class="h2_ttl">登録鳶・土工基幹技能者</h2>
+      <div class="kikan-content">
+        <p class="kikan-content-img"><img src="/wp/nittobiren/wp-content/themes/nittobiren/image/kikan/kikan.png" alt=""></p>
+        <p class="kikan-txt">「登録鳶・土工基幹技能者」は、建設業法に基づき国土交通省により認定された資格です。
+          日鳶連では、（一社）日本建設躯体工事業団体連合会（略称「日本躯体」）
+          との連名により、平成20年12月12日付けで登録認可されました。
+          これにより、今後開催する認定講習の修了者は、
+          経営事項審査の加点（3点）対象とされます。
+          ※特例講習は平成24年度で終了しました。</p>
+      </div>  
+    </section>
+    <section class="document sec_inner">
+      <h2 class="h2_ttl">定款・事業・財務資料</h2>
+      <div class="docment_item">
+        <div class="C_more_btn document-more_btn">
+          <div class="C_more_btn_block">
+            <a href="" class="C_more_btn_block-link">
+              <p class="C_more_btn_block-link-txt">定款</p>
+              <p><img src="/wp/nittobiren/wp-content/themes/nittobiren/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+            </a>
+          </div>
+        </div>
+        <div class="C_more_btn document-more_btn">
+          <div class="C_more_btn_block">
+            <a href="" class="C_more_btn_block-link">
+              <p class="C_more_btn_block-link-txt">事業報告書</p>
+              <p><img src="/wp/nittobiren/wp-content/themes/nittobiren/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="docment_item">
+        <div class="C_more_btn document-more_btn">
+          <div class="C_more_btn_block">
+            <a href="" class="C_more_btn_block-link">
+              <p class="C_more_btn_block-link-txt">事業計画書</p>
+              <p><img src="/wp/nittobiren/wp-content/themes/nittobiren/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+            </a>
+          </div>
+        </div>
+        <div class="C_more_btn document-more_btn">
+          <div class="C_more_btn_block">
+            <a href="" class="C_more_btn_block-link">
+              <p class="C_more_btn_block-link-txt">財務資料</p>
+              <p><img src="/wp/nittobiren/wp-content/themes/nittobiren/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div><!-- /l_inner -->
+</div><!-- flex_inner -->
+<?php get_footer(); ?>
