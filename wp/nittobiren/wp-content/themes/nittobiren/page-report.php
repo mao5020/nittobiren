9f4f1a8304da2de7ee1ede3b42a0acd6e52ccd00
kenta <>
2022-03-20 16:59:51 +0900

    report追加

---
+++ wp/nittobiren/wp-content/themes/nittobiren/page-report.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+  <div class="page_cover">
+    <h1 class="page_ttl">概要</h1>
+  </div>
+<div class="flex_inner">
+  <?php get_sidebar();?>
+  <div class="l_inner">
+    <section class="report sec_inner">
+      <h2 class="h2_ttl">技能五輪</h2>
+      <p class="report-txt">第５９回技能五輪全国大会の実施結果</p>
+      <p class="report-img"><img src="<?php echo get_theme_file_uri()?>/image/report/技能五輪.png" alt=""></p>
+      <p class="report-img-txt">第５９回技能五輪全国大会出場選手</p>
+    </section>
+    
+    <div class="sec_inner">
+      <p class="report-content-txt">日鳶連では、第５９回技能五輪全国大会「とび職種」競技に協力しました。</p>
+      <div class="report-content">
+        <div class="report-content-left">
+          <ul class="report-box">
+            <li class="report-box-list">日　　程：2021年12月18日（土）～19日（日）</li>
+            <li class="report-box-list">競技会場：東京都江東区「東京港湾局用地」</li>
+            <li class="report-box-list">詳　　細：<a href="https://www.javada.or.jp/jigyou/gino/zenkoku/index.html">中央職業能力開発協会ホームページ</a></li>
+            <li class="report-box-list">競技課題：「コの字方高床式切妻小屋組」</li>
+          </ul>
+          <ul class="report-box-second">
+            <li class="report-box-list">【とび職種入賞者】</li>
+            <li class="report-box-list">金　賞：小山　空我（東京都）</li>
+            <li class="report-box-list">銅　賞：藤原　瑠偉（広島県）</li>
+            <li class="report-box-list">銅　賞：岡田　　司（山口県）</li>
+          </ul>
+          <ul class="report-box-second">
+            <li class="report-box-list">【成績発表】</li>
+            <li class="report-box-list">とび部門以外の成績は、中央職業能力開発協会技能五輪詳細ページをご覧下さい。</li>
+          </ul>
+        </div>   
+        <div class="report-content-right">
+          <p class="report-img-txt">競技課題取組の様子</p>
+          <p><img src="<?php echo get_theme_file_uri()?>/image/report/技能五輪作業.png" alt=""></p>
+        </div> 
+      </div>         
+    </div>
+    
+    <section class="report sec_inner">
+      <h2 class="h2_ttl">日鳶連新聞</h2>
+      <p class="report-sinbun-txt">日鳶連では、都道府県連、国土交通省・厚生労働省はじめ関係官庁、関係企業等から投稿や資料提供をいただき、<br>
+        内容の検討、研究を重ね、業界に関する情報及び各県連等の動きについて全国の会員他、<br>
+        関係団体に機関紙「日鳶連新聞」を発行し配布しております。<br>
+        今年度発行分については<a href="http://www.nittobiren.or.jp/kain/kaiin.html">会員のページ</a>よりご覧ください。</p>
+    </section>
+  </div><!-- /l_inner -->
+</div><!-- flex_inner -->
+<?php get_footer(); ?>
