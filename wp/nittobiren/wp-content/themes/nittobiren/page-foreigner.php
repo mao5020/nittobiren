dfec8635b655d63038f7c1b707b528da2f4ad2dc
kenta <>
2022-03-22 23:35:41 +0900

    foreigner追加

---
+++ wp/nittobiren/wp-content/themes/nittobiren/page-foreigner.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+<div class="page_cover">
+  <h1 class="page_ttl">外国人受入事業</h1>
+</div>
+<div class="flex_inner">
+  <?php get_sidebar();?>
+  <div class="l_inner"> 
+    <section class="foreigner sec_inner">
+      <h2 class="h2_ttl">外国人受入事業</h2>
+      <p class="foreigner-txt">
+        日鳶連では令和２年１月31日付けで厚生労働省より外国人実習生に係る監理団体の許可が承認されたことを受けて、<br>
+        外国人技能実習生の受入事業を行っています。<br>
+        また、令和２年４月１日付けで国土交通省所管による建設分野における外国人材の受入事業に<br>
+        「とび職種」が新たに加わったことを受け、特定技能での外国人材の受入事業も行っています。<br>
+        外国人技能実習生及び特定技能に伴う外国人就労者の受入れをご希望の事業所様は<br>
+        下記の該当するお問合せよりご連絡ください。
+      </p>
+      <div class="kikan-corse">
+        <div class="C_more_btn kikan-corse-btn">
+          <div class="C_more_btn_block">
+            <a href="" class="C_more_btn_block-link">
+              <p class="C_more_btn_block-link-txt">外国人技能実習生受入れのお問い合わせ</p>
+              <p><img src="<?php echo get_theme_file_uri()?>/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+            </a>
+          </div>
+        </div>
+        <div class="C_more_btn kikan-corse-btn">
+          <div class="C_more_btn_block">
+            <a href="" class="C_more_btn_block-link">
+              <p class="C_more_btn_block-link-txt">外国人特定技能受入れのお問い合わせ</p>
+              <p><img src="<?php echo get_theme_file_uri()?>/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div><!-- /l_inner -->
+</div><!-- flex_inner -->
+<?php get_footer(); ?>
