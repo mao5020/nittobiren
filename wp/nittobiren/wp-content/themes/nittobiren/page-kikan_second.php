723b3138548fc42b84371ebd71a5caddc55bd731
kenta <>
2022-03-23 00:15:38 +0900

    kikan-second

---
+++ wp/nittobiren/wp-content/themes/nittobiren/page-kikan_second.php
@@ -0,0 +1,179 @@
+<?php get_header(); ?>
+<div class="page_cover">
+  <h1 class="page_ttl">登録鳶・土工基幹技能者講習会／更新</h1>
+</div>
+<div class="flex_inner">
+  <?php get_sidebar();?>
+  <div class="l_inner">
+    <section class="sec_inner">
+      <h2 class="h2_ttl">更新講習</h2>
+      <p class="kikan_second-txt">
+        登録鳶・土工基幹技能者は、有効期間5年のため、更新にあたり講習会の受講が必要となります。<br>
+        修了者番号・取得日は、<a class="kikan_second-txt-kink" href="http://www.yoi-kensetsu.com/kikan/data_search.php?no=16" target="_blank">登録基幹技能者データベース</a>により確認することが出来ます。<br>
+        ※なお、有効満了日は5年後の取得日前日です。<br>
+        （修了年月日：2018年4月1日⇒有効期限：2023年3月31日）
+      </p>
+      <h4 class="h4_ttl">開催日程</h4>
+      <table class="C_kikan-table" border="1">
+        <tbody>
+          <tr>
+            <td> 開催日</td>
+            <td>開催地</td>
+            <td>定員</td>
+            <td>会場</td>
+            <td>住所</td>
+            <td>申込期間</td>
+          </tr>
+          <tr>
+            <td rowspan="3" >2022年<br>6月11日(土)～12日(日)</td>
+            <td>東京</td>
+            <td>40</td>
+            <td><a href="http://www.jspmi.or.jp/kaigishitsu/access.html">機会振興会館6D-4</a></td>
+            <td>東京都港区芝公園3-5-8</td>
+            <td rowspan="3" >2022年1月1日(土)～2022年5月20日(金)</td>
+          </tr>
+          <tr>
+            <td>神戸</td>
+            <td>40</td>
+            <td><a href="http://www.kobekhall.com/akusesu/">神戸市教育会館501会議室</a></td>
+            <td>神戸市中央区<br>
+            中山手通4丁目10番5号</td>
+          </tr>
+          <tr>
+            <td>鹿児島</td>
+            <td>40</td>
+            <td><a href="https://www.houzanhall.com/access/">宝山<br>ホール第6</a></td>
+            <td>鹿児島県鹿児島市山下町5-3</td>
+          </tr>
+          <tr>
+            <td rowspan="3" >2022年<br>
+            9月10日(土)～11日(日)</td>
+            <td>帯広</td>
+            <td>40</td>
+            <td><a href="http://obi-syokuno.ac.jp/access.html">帯広地方職業能力開発協会<br>教室4･5</a></td>
+            <td>帯広市西22条北2丁目29番4号<br>
+            帯広職業能力開発センター内</td>
+            <td rowspan="3" >2022年1月1日(土)～2022年8月19日(金)</td>
+          </tr>
+          <tr>
+            <td>石川</td>
+            <td>40</td>
+            <td><a href="http://www.ishikawa-seisoken.jp/newkoutuuannai.html">ユースパルいしかわ<br>
+            研修室1</a></td>
+            <td>石川県金沢市常盤町212-1</td>
+          </tr>
+          <tr>
+            <td>大坂</td>
+            <td>40</td>
+            <td><a href="http://www.pss-kasikaigi.com/access/">堺市<br>
+            東洋ビル<br>
+            4F-7</a></td>
+            <td>大阪府堺市堺区北花田口町3丁1番15号</td>
+          </tr>
+          <tr>
+            <td rowspan="3" >2022年<br>
+            11月12日(土)～13日(日)</td>
+            <td>東京</td>
+            <td>40</td>
+            <td><a href="http://www.jspmi.or.jp/kaigishitsu/access.html">機械振興会館6D-4</a></td>
+            <td>東京都港区芝公園3-5-8</td>
+            <td rowspan="3" >2022年1月1日(土)～2022年10月21日(金)</td>
+          </tr>
+          <tr>
+            <td>広島</td>
+            <td>40</td>
+            <td><a href="https://intelligent-hotel.co.jp/oc/t_fronts/access">広島<br>
+            オフィスセンター 第1</a></td>
+            <td>広島県広島市南区東荒神町3-35</td>
+          </tr>
+          <tr>
+            <td>札 幌</td>
+            <td>40</td>
+            <td><a href="https://www.google.co.jp/maps/place/%E3%80%92003-0807+%E5%8C%97%E6%B5%B7%E9%81%93%E6%9C%AD%E5%B9%8C%E5%B8%82%E7%99%BD%E7%9F%B3%E5%8C%BA%E8%8F%8A%E6%B0%B4%EF%BC%97%E6%9D%A1%EF%BC%92%E4%B8%81%E7%9B%AE%EF%BC%92%E2%88%92%EF%BC%92%EF%BC%90/@43.061394,141.379452,17z/data=!4m5!3m4!1s0x5f0b2962e8ba0c8b:0x494384acdab33312!8m2!3d43.061146!4d141.379452?hl=ja">菊水地区会館</a></td>
+            <td>札幌市白石区菊水7条2丁目2-20</td>
+          </tr>
+          <tr>
+            <td rowspan="3" >2023年<br>
+            2月11日(土)～12日(日)</td>
+            <td>山形</td>
+            <td>40</td>
+            <td><a href="https://www.city.sagae.yamagata.jp/kurashi/shisetsu/bunkashogaigakusyu/gijutukouryuplaza.html">寒川江市技術交流プラザ<br>
+            (予定)</a></td>
+            <td>寒河江市中央工業団地153の1</td>
+            <td rowspan="3" >2022年1月1日(土)～2023年1月20日(金)</td>
+          </tr>
+          <tr>
+            <td>大坂</td>
+            <td>40</td>
+            <td><a href="大阪府堺市堺区北花田口町3丁1番15号">堺市<br>東洋ビル<br>4F-7</a></td>
+            <td>大阪府堺市堺区北花田口町3丁1番15号</td>
+          </tr>
+          <tr>
+            <td>大分</td>
+            <td>40</td>
+            <td><a href="http://www.horutohall-oita.jp/access/">ホルトホール大分<br>
+            (予定)</a></td>
+            <td>大分県大分市金池南一丁目5番1号</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="kikan-table-note">
+        <p class="kikan-table-note-txt">※開催予定、人数により会場が変更される場合がございます。ホームページにてご確認下さい。</p>
+        <p class="kikan-table-note-txt">※受講申込者数が10名未満の場合は開催中止となる場合があります。</p>
+        <p class="kikan-table-note-txt">※定員になり次第受付終了となります。</p>
+      </div>
+      <div class="C_more_btn">
+        <div class="C_more_btn_block test">
+          <a href="" class="C_more_btn_block-link">
+            <p class="C_more_btn_block-link-txt">般社団法人日本建設躯体工事業団体連合会　開催についてはこちら。</p>
+            <p><img src="<?php echo get_theme_file_uri()?>/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+          </a>
+        </div>
+      </div>
+    </section>
+    <section class="sec_inner">
+      <h4 class="h4_ttl">受講資格</h4>
+      <p class="activities-txt">下記条件を全てを充たす必要がある。</p>
+      <ul class="kikan-box">
+        <li class="kikan-box-list">10年以上のとび・土工工事の実務経験を有する者</li>
+        <li class="kikan-box-list">8年以上のとび・土工工事の職長経験のある者</li>
+        <li class="kikan-box-list">下記の条件を充たしている者<br> - 安衛法第60条に基づき安衛則第40条第2項で定める職長教育を受講し、修了後5年以上経過している者</li>
+        <li class="kikan-box-list">下記の資格のいずれかを有する者<br> - 1級とび技能士<br> - 1・2級建築士施工管理技師<br> - 1・2級土木施工管理技士</li>
+      </ul>
+      <h4 class="h4_ttl">開催受講申込提出書類</h4>
+      <ul class="kikan-box">
+        <li class="kikan-box-list">受講申込書（事業主が証明したもの）：　[ PDF ・ Excel　／　記入例 ] </li>
+        <li class="kikan-box-list">実務経験証明書：　[ PDF ・ Excel 　／　記入例 ] <br> - 労働安全衛生法第60条に規定する職長教育修了証の写し、または事業主以外の元請の建設業者による証明書類の写し<br> - ※ただし、修了証及び証明書類は、修了後5年以上経過しているもの </li>
+        <li class="kikan-box-list">資格証の写し（上記2．(4)の資格）</li>
+        <li class="kikan-box-list">下記の資格のいずれかを有する者<br> - 1級とび技能士<br> - 1・2級建築士施工管理技師<br> - 1・2級土木施工管理技士</li>
+      </ul>
+      <h4 class="h4_ttl">講習内容</h4>
+      <ul class="kikan-box">
+        <li class="kikan-box-list">講義（10時間）　／　修了試験（45分）</li>
+      </ul>
+      <h4 class="h4_ttl">振込先口座</h4>
+      <ul class="kikan-box">
+        <li class="kikan-box-list">三井住友銀行　日比谷支店（店番632） </li>
+        <li class="kikan-box-list">普通預金　口座番号 7100898</li>
+        <li class="kikan-box-list">資格証の写し（上記2．(4)の資格）</li>
+        <li class="kikan-box-list">名義　一般社団法人日本鳶工業連合会</li>
+      </ul>
+      <h4 class="h4_ttl">受講票送付</h4>
+      <ul class="kikan-box">
+        <li class="kikan-box-list">受講申込みをされた方には、受講資格及び申請手数料入金等確認後、 「受講票」を送付する。</li>
+      </ul>
+      <h4 class="h4_ttl">助成金</h4>
+      <ul class="kikan-box">
+        <li class="kikan-box-list">当講習会は、｢人材開発支援助成金｣の対象講習会です。 詳細及び申請様式は厚生労働省ホームページをご確認ください。 申請様式はこちら</li>
+      </ul>
+      <h4 class="h4_ttl">問い合せ先及び申請書送付先</h4>
+      <ul class="kikan-box">
+        <li class="kikan-box-list">一般社団法人日本鳶工業連合会</li>
+        <li class="kikan-box-list">〒105-0011　東京都港区芝公園3-5-20　日鳶連会館内</li>
+        <li class="kikan-box-list">TEL：03-3434-8805</li>
+        <li class="kikan-box-list">※角2封筒（A4用紙用）に受講申込書等を折らずに入れ、必ず配達記録が残る方法(簡易書留、特定記録など)で郵送してください</li>
+      </ul>
+    </section>
+  </div>
+</div>
+<?php get_footer(); ?>
