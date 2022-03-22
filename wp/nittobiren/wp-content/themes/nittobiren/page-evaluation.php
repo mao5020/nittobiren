7af3ea7ee430aebb3a8906ffd4eb66e97a9ec87f
kenta <>
2022-03-22 22:18:25 +0900

    evaluationページ追加

---
+++ wp/nittobiren/wp-content/themes/nittobiren/page-evaluation.php
@@ -0,0 +1,99 @@
+<?php get_header(); ?>
+<div class="page_cover">
+  <h1 class="page_ttl">とび技能者能力評価について</h1>
+</div>
+<?php get_template_part('p-header'); ?>
+</body>
+</html>
+
+<div class="flex_inner">
+  <?php get_sidebar();?>
+  <div class="l_inner">
+    <section class="evaluation sec_inner">
+      <h2 class="h2_ttl">とび技能者能力評価基準</h2>
+      <div class="evaluation-content">
+        <p class="evaluation-txt">
+            建設技能者の能力評価制度に関する告示(平成31年国土交通省告示第460号) 及び<br>
+            建設技能者の能力評価制度に関するガイドライン(平成31年3月29日)に基づき、<br>
+            とび技能者の能力評価基準を定めております。
+        </p>
+      </div>
+      <div class="kikan-corse">
+        <div class="kikan-corse-btn"></div>
+        <div class="C_more_btn kikan-corse-btn">
+          <div class="C_more_btn_block">
+            <a href="" class="C_more_btn_block-link">
+              <p class="C_more_btn_block-link-txt">とび技能者能力評価基準詳細</p>
+              <p><img src="<?php echo get_theme_file_uri()?>/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+            </a>
+          </div>
+        </div>
+      </div>
+      
+      <h2 class="h2_ttl">とび技能者能力評価実施規程</h2>
+      <div class="evaluation-content">
+        <p class="evaluation-txt">
+            建設技能者の能力評価制度に関する告示(平成31年国土交通省告示第460号) 及び<br>
+            建設技能者の能力評価制度に関するガイドライン(平成31年3月29日)に基づき、<br>
+            とび技能者の能力評価基準を定めております。
+        </p>
+      </div>
+      <div class="kikan-corse">
+        <div class="kikan-corse-btn"></div>
+        <div class="C_more_btn kikan-corse-btn">
+          <div class="C_more_btn_block">
+            <a href="" class="C_more_btn_block-link">
+              <p class="C_more_btn_block-link-txt">とび技能者能力評価実施規程詳細</p>
+              <p><img src="<?php echo get_theme_file_uri()?>/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+            </a>
+          </div>
+        </div>
+      </div>
+      
+      <h2 class="h2_ttl">能力評価実施団体レベル判定申請書関連</h2>
+      <div class="evaluation-content">
+        <p class="evaluation-txt">
+          下記内容に対する説明書き等をここに記載、サンプルテキストサンプルテキストサンプルテキスト
+          サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
+        </p>
+      </div>
+      <div class="docment_item">
+        <div class="C_more_btn document-more_btn">
+          <div class="C_more_btn_block">
+            <a href="" class="C_more_btn_block-link">
+              <p class="C_more_btn_block-link-txt">レベル判定申請の受付について</p>
+              <p><img src="<?php echo get_theme_file_uri()?>/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+            </a>
+          </div>
+        </div>
+        <div class="C_more_btn document-more_btn">
+          <div class="C_more_btn_block">
+            <a href="" class="C_more_btn_block-link">
+              <p class="C_more_btn_block-link-txt">とび技能者レベル判定申請書等様式</p>
+              <p><img src="<?php echo get_theme_file_uri()?>/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="docment_item">
+        <div class="C_more_btn document-more_btn">
+          <div class="C_more_btn_block">
+            <a href="" class="C_more_btn_block-link">
+              <p class="C_more_btn_block-link-txt">能力評価基準</p>
+              <p><img src="<?php echo get_theme_file_uri()?>/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+            </a>
+          </div>
+        </div>
+        <div class="C_more_btn document-more_btn">
+          <div class="C_more_btn_block">
+            <a href="" class="C_more_btn_block-link">
+              <p class="C_more_btn_block-link-txt">CCUS閲覧画面の操作説明</p>
+              <p><img src="<?php echo get_theme_file_uri()?>/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div><!-- /l_inner -->
+</div><!-- flex_inner -->
+<?php get_footer(); ?>
