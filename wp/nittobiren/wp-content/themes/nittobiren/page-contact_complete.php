19ccd785d7fd0561be56580bc854a828f55db52a
mao5020 <fffmao.5252@gmail.com>
2022-05-09 23:16:26 +0900

    送信完了画面 footer調整

--- wp/nittobiren/wp-content/themes/nittobiren/page-contact_complete.php
+++ wp/nittobiren/wp-content/themes/nittobiren/page-contact_complete.php
@@ -1,25 +1,27 @@
 <?php get_header(); ?>
-<div class="page_cover">
-  <h1 class="page_ttl">お問い合わせ</h1>
-</div>
-<div class="single_inner">
-  <div class="contact">
-    <h2 class="contact-ttl">お問い合わせ完了</h2>
+<div class="wrapper">
+  <div class="page_cover">
+    <h1 class="page_ttl">お問い合わせ</h1>
+  </div>
+  <div class="single_inner">
     <div class="contact">
-      <p class="contact-txt">
-        この度は、お問合せいただき、誠にありがとうございました。<br>
-        お送りいただきました内容を確認の上、折り返しご連絡させていただきます。<br>
-        今しばらくお待ちくださいますようよろしくお願い申し上げます。<br>
-      </p>  
-      <p class="contact-txt">
-        なお、しばらくたっても返信、返答がない場合は、<br>
-        入力いただいたメールアドレスに誤りがあった可能性がございます。<br>
-        再度ご確認の上、もう一度フォームよりお問合せ頂きますようお願い申し上げます。
-      </p>
-    </div>
-  </div>  
-</div>
-<div class="contact-button">
-  <a href="<?php echo get_site_url(); ?>/top">TOPに戻る</a>
+      <h2 class="contact-ttl">お問い合わせ完了</h2>
+      <div class="contact">
+        <p class="contact-txt">
+          この度は、お問合せいただき、誠にありがとうございました。<br>
+          お送りいただきました内容を確認の上、折り返しご連絡させていただきます。<br>
+          今しばらくお待ちくださいますようよろしくお願い申し上げます。<br>
+        </p>  
+        <p class="contact-txt">
+          なお、しばらくたっても返信、返答がない場合は、<br>
+          入力いただいたメールアドレスに誤りがあった可能性がございます。<br>
+          再度ご確認の上、もう一度フォームよりお問合せ頂きますようお願い申し上げます。
+        </p>
+      </div>
+    </div>  
+  </div>
+  <div class="contact-button">
+    <a href="<?php echo get_site_url(); ?>/top">TOPに戻る</a>
+  </div>
 </div>
 <?php get_footer(); ?>
