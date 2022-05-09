7d2ce18bbe0d9efeddad106648fbab6f10153fb4
mao5020 <fffmao.5252@gmail.com>
2022-05-10 07:34:02 +0900

    contact7 修正

--- wp/nittobiren/wp-content/themes/nittobiren/functions.php
+++ wp/nittobiren/wp-content/themes/nittobiren/functions.php
@@ -4,12 +4,5 @@ function twpp_deregister_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'twpp_deregister_styles' );
 
-// // クエリカスタマイズ【お知らせ新着表示】
-// function foo_modify_main_queries ( $query ) {
-//   if ( ! is_admin() && $query->is_main_query() ) { // 管理画面以外かつメインクエリーを対象とする
-//     if ( $query->is_home() ) {
-//       $query->set( 'post_type', array('news')); // 投稿とカスタム投稿（blog）を含める
-//     }
-//   }
-// }
-// add_action( 'pre_get_posts', 'foo_modify_main_queries' );
+add_filter( 'wpcf7_load_js', '__return_false' );
+add_filter( 'wpcf7_support_html5_fallback', '__return_true' );
