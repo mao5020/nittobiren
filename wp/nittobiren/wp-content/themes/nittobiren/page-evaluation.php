b2a586a09016548cdc28d0107cad77c1daeb2e7f
mao5020 <fffmao.5252@gmail.com>
2022-05-17 11:12:32 +0900

    URL変更

--- wp/nittobiren/wp-content/themes/nittobiren/page-evaluation.php
+++ wp/nittobiren/wp-content/themes/nittobiren/page-evaluation.php
@@ -64,7 +64,7 @@
       <div class="kikan-corse">
         <div class="C_more_btn kikan-corse-btn">
           <div class="C_more_btn_block">
-            <a href="<?php echo get_theme_file_uri();?>/image/evaluation/%E3%81%A8%E3%81%B3%E6%8A%80%E8%83%BD%E8%80%85%E3%83%AC%E3%83%99%E3%83%AB%E5%88%A4%E5%AE%9A%E7%94%B3%E8%AB%8B%E3%81%AE%E5%8F%97%E4%BB%98%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6.pdf" target="_blank" rel="noopener" class="C_more_btn_block-link">
+            <a href="<?php echo get_theme_file_uri();?>/image/evaluation/%E3%81%A8%E3%81%B2%E3%82%99%E6%8A%80%E8%83%BD%E8%80%85%E3%83%AC%E3%83%98%E3%82%99%E3%83%AB%E5%88%A4%E5%AE%9A%E7%94%B3%E8%AB%8B%E3%81%AE%E5%8F%97%E4%BB%98%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6.pdf" target="_blank" rel="noopener" class="C_more_btn_block-link">
               <p class="C_more_btn_block-link-txt">レベル判定申請の受付について</p>
               <p><img class="C_more_btn_block-link-img" src="<?php echo get_theme_file_uri()?>/image/common/more.png" alt="" class="C_more_btn_block-link-img"></p>
             </a>
