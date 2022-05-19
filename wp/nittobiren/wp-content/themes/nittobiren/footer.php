c1e9124994d6a0dc9095423ddd4e364a7da29682
mao5020 <fffmao.5252@gmail.com>
2022-05-20 03:05:21 +0900

    script修正

--- wp/nittobiren/wp-content/themes/nittobiren/footer.php
+++ wp/nittobiren/wp-content/themes/nittobiren/footer.php
@@ -2,6 +2,5 @@
       <p class="footer-txt">copyright©2017 nittobiren  all rights reserved.</p>
     </footer>
     <?php wp_footer(); ?>
-    <script type="text/javascript">wpcf7.cached = 0; </script>
   </body>
 </html>
