f67ca85bf042f5005d5480a7ca1827bf32c18f51
mao5020 <fffmao.5252@gmail.com>
2022-06-16 18:47:09 +0900

    特定外国人制度pdf追加

--- wp/nittobiren/wp-content/themes/nittobiren/page-foreigner__test.php
+++ wp/nittobiren/wp-content/themes/nittobiren/page-foreigner__test.php
@@ -10,9 +10,9 @@
     <p class="test-txt">建設分野における特定技能外国人の受け入れ制度は、平成31年4月より開始されており、<br>現在受け入れが認められrた11職種で特定技能1号評価試験の実施が計画されています。<br>特定技能の在留資格を取得するためには、特定技能評価試験に合格することに加えて、日本語試験に合格することが必要です。<br>建設分野における特定技能1号評価試験は、国交相が定めた「建設分野特定技能評価試験実施要領」に従い、<br>一般社団法人建設技能人材機構（JAC）が、学科試験及び実技試験により行います。</p>
     <p class="test-txt">※下記リンクより「教本テキスト」「学科サンプル問題」「実技サンプル問題」をご覧いただけます。</p>
     <ul class="test-box">
-      <li class="test-box-list"><a class="test-box-list-link" href="">教本テキスト</a></li>
-      <li class="test-box-list"><a class="test-box-list-link" href="">学科サンプル問題</a></li>
-      <li class="test-box-list"><a class="test-box-list-link" href="">実技試験問題</a></li>
+      <li class="test-box-list"><a class="test-box-list-link" href="<?php echo get_theme_file_uri();?>/document/foreigner__test/to_tutorial_all.pdf" target="_blank">教本テキスト</a></li>
+      <li class="test-box-list"><a class="test-box-list-link" href="<?php echo get_theme_file_uri();?>/document/foreigner__test/to_sample.pdf" target="_blank">学科サンプル問題</a></li>
+      <li class="test-box-list"><a class="test-box-list-link" href="<?php echo get_theme_file_uri();?>/document/foreigner__test/to_skilltest.pdf" target="_blank">実技試験問題</a></li>
     </ul>
     </section>
   </div>
